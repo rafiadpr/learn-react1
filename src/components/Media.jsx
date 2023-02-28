2463abf529a1fd13f82bb987b6bb73c6befb051b
rafiadpr <rafi_prasetya_30rpl@student.smktelkom-mlg.sch.id>
2023-02-28 09:42:50 +0700

    update

---
+++ src/components/Media.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class Media extends React.Component{
+    render(){
+        return (
+            <div className={'row alert alert-' + this.props.type}>
+                <div className="col col-auto">
+                    <img src={process.env.PUBLIC_URL+'../../public/image'+this.props.image} className='mr-3' width='100 px' alt='media' />
+                </div>
+                <div className="col col-auto text-left">
+                    <h4 className="mt-0">{this.props.title}</h4>
+                    <p>{this.props.children}</p>
+                </div>
+            </div>
+        )
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'orange' : '#f8831c',
+      'abu' : 'd9d9d9',
+    },
   },
   plugins: [],
 }
